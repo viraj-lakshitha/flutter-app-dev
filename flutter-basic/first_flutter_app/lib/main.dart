6fe4b7d43208302a19019e985a1faa97db524426
viraj-lakshitha <virajlakshithabandara@gmail.com>
2021-05-15 08:15:57 +0530

    Stateless and Stateful Widgets for HotReloading

--- flutter-basic/first_flutter_app/lib/main.dart
+++ flutter-basic/first_flutter_app/lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello World !'),
+  home: Home(),
 ));
 
+class Home extends StatelessWidget { // Enable Hot-Reload
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        title: Text('My First Application'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(
+          'Hello World !',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: 'NewsReader',
+            letterSpacing: 2.0,
+            color: Colors.orange,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { print('Button Clicked !'); },
+        child: Text('Click'),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
 
+
+/// Documentation : https://api.flutter.dev/flutter/material/Scaffold-class.html
