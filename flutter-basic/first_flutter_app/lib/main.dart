50246d2bdaad5edc8fd74e46c45cdd6216a17434
viraj-lakshitha <virajlakshithabandara@gmail.com>
2021-05-15 08:42:19 +0530

    update : Material Design Icons

--- flutter-basic/first_flutter_app/lib/main.dart
+++ flutter-basic/first_flutter_app/lib/main.dart
@@ -15,12 +15,11 @@ class Home extends StatelessWidget { // Enable Hot-Reload
         backgroundColor: Colors.green,
       ),
       body: Center(
-        // child: Image(
-        //   // image: NetworkImage('https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg'),
-        //   image: AssetImage('./assets/image-2.jpg') // update pubspec.yaml file
-        //
-        // ),
-        child: Image.asset('assets/image-1.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.green,
+          size: 50.0,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () { print('Button Clicked !'); },
