83424661a3f88f2d6eea80ab1c15d3e764e1ab25
viraj-lakshitha <virajlakshithabandara@gmail.com>
2021-05-16 11:07:05 +0530

    update: custome card widget

--- flutter-basic/concepts_flutter/lib/main.dart
+++ flutter-basic/concepts_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quotes.dart';
 
 void main() => runApp(
     MaterialApp(
@@ -12,8 +13,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<Quote> quotes = [
+    Quote(text: 'Life Hacks 1', author: 'Author 1'),
+    Quote(text: 'Life Hacks 2', author: 'Author 2'),
+    Quote(text: 'Life Hacks 3', author: 'Author 3')
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0,16.0,16.0,0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              '${quote.text}',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              '${quote.author}',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map( (quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
   }
 }
---
+++ flutter-basic/concepts_flutter/lib/quotes.dart
@@ -0,0 +1,8 @@
+class Quote {
+
+  // Properties
+  late String text;
+  late String author;
+
+  Quote({required this.text, required this.author});
+}
